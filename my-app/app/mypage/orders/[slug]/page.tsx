b98b69fb7fd6ada740eed4d876e50857f05b8947
Hamadalishah <hammadalis459@gmail.com>
2024-12-31 12:43:56 +0500

    making the order dynamic routes

--- my-app/app/mypage/orders/[slug]/page.tsx
+++ my-app/app/mypage/orders/[slug]/page.tsx
@@ -1,19 +1,16 @@
 // app/mypage/orders/[slug]/page.tsx
 "use client"
 import React from 'react';
-import { useParams } from 'next/navigation'; // UseRouter().query.slug for `pages` directory
+import { useParams } from 'next/navigation'; 
 import MainContent from './_component/main-content';
 import PageLayout from '@/app/components/sliderNavebar';
 import { slugData } from './_component/slugData';
 
 const SlugPage = () => {
-    const { slug } = useParams(); // Retrieves the `slug` parameter from the URL
-  
-    // Ensure slug is a string
+    const { slug } = useParams(); 
     const slugKey = Array.isArray(slug) ? slug[0] : slug;
   
    
-    // Fallback for unknown slugs
     const content = slugData[slugKey as 'processing' | 'in-transit' | 'delivery-completed' | 'cancelled' | 'refunded'] || {
       title: 'Unknown Order Status',
       description: 'The selected order status is not recognized.',
