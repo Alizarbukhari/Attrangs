8a70e3ad49b0d3eadc67df4f7af73cfc19f1079c
Hamadalishah <hammadalis459@gmail.com>
2025-01-04 11:31:59 +0500

    setup backend regiter.y file

--- Attrangs_backend/app/app/crud/crud.py
+++ Attrangs_backend/app/app/crud/crud.py
@@ -6,11 +6,17 @@ from passlib.context import CryptContext  # type: ignore
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+def verify_password_hash(plain_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+        return pwd_context.hash(password)
+
 def get_user_by_username(db: Session, userNameId: str):
-    return db.query(User_Register).filter(User_Register.user_name_id == userNameId).first()
+    return db.query(User_Register).filter(User_Register.user_name_id == userNameId).first() # type: ignore
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(User_Register).filter(User_Register.email == email).first()
+    return db.query(User_Register).filter(User_Register.email == email).first() # type: ignore
 
 def create_user(db: Session, user: User_Create_Register):
     hashed_password = pwd_context.hash(user.password)
@@ -42,7 +48,7 @@ def authenticate_user(db: Session, email: str, password: str):
 
 def update_user_data(db: Session, user_id: int, update_data: User_Update):
     # db.execute returns a Result object, we need the actual user
-    db_user = db.scalar(select(User_Register).where(User_Register.id == user_id))
+    db_user = db.scalar(select(User_Register).where(User_Register.id == user_id)) # type: ignore
     if not db_user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
